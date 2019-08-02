8125689ef758414beafddca1251b8c74a4858c3e
nomadjimbob <james.collins@outlook.com.au>
2019-08-02 11:22:08 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ firebase.initializeApp(firebaseConfig);
 
 //var database = firebase.database();
 var ref = firebase.database().ref('users');
-var obj = {password: "456"};
-var tdxt= "hi";
+var obj = {{userid:"90567", password: "456"},{userid:"90999", password: "123"}};
 ref.push(obj);   // Creates a new ref with a new "push key"
 ref.set(obj);    // Overwrites the path
 ref.update(obj); // Updates only the specified attributes 
