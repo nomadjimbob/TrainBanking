114fa0dd0e957646889a487086447a10646ee76c
nomadjimbob <james.collins@outlook.com.au>
2019-08-02 13:11:57 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,16 +17,27 @@ ref.set(obj);    // Overwrites the path
 ref.update(obj); // Updates only the specified attributes 
 */
 
+var transRef = firebase.database().ref('transaction/');
+var itemRef = transRef.push();
+itemRef.set({amount:96.58,description:"pay james"});
+
+var obj = {password: "456"};
+ref.push(obj);   // Creates a new ref with a new "push key"
+ref.set(obj);    // Overwrites the path
+ref.update(obj); // Updates only the specified attributes 
+
+
 
 
 //var usersRef = firebase.app().database().ref().child('users');
 //var usersRef = firebase.app().database().ref().child('users').orderByKey();
-var usersRef = firebase.app().database().ref('/users/').orderByChild('password').equalTo("999");
+//var usersRef = firebase.app().database().ref('/users/').orderByChild('password').equalTo("999");
 
-
+/*
 usersRef.once('value', function (snap) {
  snap.forEach(function (childSnap) {
   console.log('user', childSnap.val());
   console.log('key', childSnap.key);
  });
 });
+*/
