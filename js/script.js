cee7edbb45b44e7d6fb97737c4a144bf29ad7c20
nomadjimbob <james.collins@outlook.com.au>
2019-08-02 13:33:25 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ usersRef.once('value', function (snap) {
 */
 
 function pageHome() {
-	$('#main').html("<div class=\"jumbotron\">\
+	$('main').html("<div class=\"jumbotron\">\
     <div class=\"container\">\
       <h1 class=\"display-3\">Hello, world!</h1>\
       <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>\
