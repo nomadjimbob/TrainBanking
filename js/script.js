957bc475a0a0bc64fb53bf4390aa022accd7874f
nomadjimbob <james.collins@outlook.com.au>
2019-08-02 10:59:10 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 //var database = firebase.database();
-var ref = firebase.app().database().ref('users');
+var ref = firebase.database().ref('users');
 var obj = {password: "123"};
 ref.push(obj);   // Creates a new ref with a new "push key"
 ref.set(obj);    // Overwrites the path
