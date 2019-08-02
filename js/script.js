4514cb68ba74bf432957846f6846a024ef169787
nomadjimbob <james.collins@outlook.com.au>
2019-08-02 10:55:57 +1000

    Moved JS to external file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyAc9LbJPcDbsMgwPMm1Eh3Q5SWYmlFk4nw",
+	authDomain: "gointernetbanking-98ca6.firebaseapp.com",
+	databaseURL: "https://gointernetbanking-98ca6.firebaseio.com",
+	projectId: "gointernetbanking-98ca6",
+	storageBucket: "",
+	messagingSenderId: "77031026372",
+	appId: "1:77031026372:web:a70b3d8aa43ca797"
+};
+firebase.initializeApp(firebaseConfig);
+
+//var database = firebase.database();
+var ref = firebase.app().database().ref();
+var obj = {someAttribute: true};
+ref.push(obj);   // Creates a new ref with a new "push key"
+ref.set(obj);    // Overwrites the path
+ref.update(obj); // Updates only the specified attributes 
